fd9e4a4d377218b4631632b2d83875886a6c8772
rokag3@gmail.com <rokag3@gmail.com>
2023-06-30 16:42:18 +0900

    GCE 특정 인스턴스 데이터 가져오기 완료.

--- GCE_API_Test/Program.cs
+++ GCE_API_Test/Program.cs
@@ -1,15 +1,45 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
 using System.Net.Http.Headers;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Compute.v1;
+using Google.Apis.Compute.v1.Data;
+using System.Net.Sockets;
 
 namespace GCE_API_Test
 {
     internal class Program
     {
+        static GoogleCredential cred = GoogleCredential.FromFile(@"C:\BizData\source\GCE_API_Test\GCE_API_Test\GitIgnore\cred.json");
+        static string _projectId = "rpm-2023";
+        static string _zone = "asia-northeast3-a";
+        static string _instanceName = "instance-1";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //var credential = cred.CreateScoped("https://www.googleapis.com/auth/compute-engine");
+
+            ComputeService computeService = new ComputeService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = cred,
+                //ProjectId = _projectId
+                ApplicationName = "Compute Engine API"
+            });
+
+            // 인스턴스 정보 가져오기
+            InstancesResource.GetRequest getRequest = computeService.Instances.Get(_projectId, _zone, _instanceName);
+            Instance response = getRequest.Execute();
+
+            // 인스턴스의 SKU 정보 추출
+            string skuId = "";
+            //skuId = response.Scheduling.NodeAffinities[0].Values[0];
+            
+            // 결과 출력
+            Console.WriteLine("id: " + response.Id.ToString());
+            Console.WriteLine("skuId: " + skuId);
         }
-
-
     }
 }
