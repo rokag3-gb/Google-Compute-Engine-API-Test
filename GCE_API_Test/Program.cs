070e32d088d1ba40ab3277c8ef4247c12ca1b30e
rokag3@gmail.com <rokag3@gmail.com>
2023-06-30 15:26:29 +0900

    초기 커밋.

---
+++ GCE_API_Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace GCE_API_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+    }
+}
